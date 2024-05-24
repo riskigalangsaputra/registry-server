098faf718fef90763ea149eb96589b20f96171ef
riskigalangsaputra <riskigalangsaputra@gmail.com>
2024-05-24 11:20:25 +0700

    create project and create eureka server for load balancer

---
+++ src/test/java/id/kingra/registry_server/RegistryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package id.kingra.registry_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
